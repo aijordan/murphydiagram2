0f40dad4b598350356051b681a3459d2a3542aeb
Fabian <fabian.krueger83@gmail.com>
2017-07-06 16:02:15 +0200

    updates (implement preliminary sd function)

--- R/methods_murphydiag_diff.R
+++ R/methods_murphydiag_diff.R
@@ -4,6 +4,7 @@
 #' 
 #' @param x an object inheriting from the class \code{murphydiag}.
 #' @param main an overall title for the plot: see \code{\link{title}}.
+#' @param level_ci level for confidence interval
 #' @inheritParams graphics::matplot
 #' 
 #' @seealso \code{\link{murphydiag_diff}},
@@ -18,7 +19,9 @@ plot.murphydiag_diff <- function(x, type = "l",
                                  ylim = NULL,
                                  main = NULL,
                                  xlab = NULL,
-                                 ylab = NULL, ...) {
+                                 ylab = NULL, 
+                                 level_ci = 0.95, 
+                                 ...) {
   m <- x
   class(m) <- "murphydiag"
   if (is.null(main)) main <- sprintf("%s vs %s", names(m$x)[1L], names(m$x)[2L])
@@ -44,15 +47,30 @@ plot.murphydiag_diff <- function(x, type = "l",
   yr_main <- fsfun(tr, right = TRUE)
   
   yl <- yl_main %>% sapply(colMeans) %>% t %>% (function(z) z[, 1L] - z[, 2L])
+  yl_sd <- yl_main %>% lapply(function(z) z[, 1L] - z[, 2L]) %>% 
+    sapply(function(z) sd(z)/sqrt(length(z))) 
   yr <- yr_main %>% sapply(colMeans) %>% t %>% (function(z) z[, 1L] - z[, 2L])
+  yr_sd <- yr_main %>% lapply(function(z) z[, 1L] - z[, 2L]) %>% 
+    sapply(function(z) sd(z)/sqrt(length(z))) 
   
   n <- length(tt) + 1L
   interleaved <- rep(1:n, each = 2L) + rep(c(0L, n), n)
   xx <- c(tl, tr)[interleaved]
   yy <- c(yl, yr)[interleaved]
+  a_lb <- 0.5*(1-level_ci)
+  yy_lb <- c(yl + qnorm(a_lb)*yl_sd, yr + qnorm(a_lb)*yr_sd)[interleaved]
+  a_ub <- 0.5 + 0.5*level_ci
+  yy_ub <- c(yl + qnorm(a_ub)*yl_sd, yr + qnorm(a_ub)*yr_sd)[interleaved]
+  
+  if (is.null(ylim)){
+    ylim <- range(yy_lb, yy_ub)
+  }
   
   matplot(xx, yy, type = type, xlim = xlim, ylim = ylim,
           main = main, xlab = xlab, ylab = ylab, ...)
+  polygon(c(xx, rev(xx)), c(yy_ub, rev(yy_lb)), col = "grey", 
+          border = NA)
+  lines(xx, yy)
   abline(h = 0, lty = 2)
   
   invisible(m)
