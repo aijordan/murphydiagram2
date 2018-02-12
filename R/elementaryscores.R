149faefad186324fb8c90fbee6bff9a6d7010b58
aijordan <a@jordan-etc.de>
2018-02-12 18:08:58 +0100

    elementary scores / remove duplication

--- R/elementaryscores.R
+++ R/elementaryscores.R
@@ -2,8 +2,6 @@
 #' 
 #' Functions for the computation of the elementary scores.
 #' 
-#' @param a an object convertible by \code{as.vector}
-#'  containing alpha levels in (0, 1).
 #' @param t an object convertible by \code{as.vector}
 #'  containing treshold values.
 #' @param x an object convertible by \code{as.matrix}
@@ -11,156 +9,78 @@
 #'   #forecast methods).
 #' @param y an object convertible by \code{as.vector}
 #'   containing observations.
+#' @param level numerical, a single value between 0 and 1.
 #' @param right logical, indicating if the scores are
-#'   right-continuous in \code{x}.
+#'   right-continuous in \code{t}.
 #' 
 #' @return Returns an array with dimension
-#'  (#observation, #forecast methods, #thresholds, #levels),
-#'  where levels is optional.
+#'  (#thresholds, #observations, #forecast methods).
 #' 
 #' @name elementaryscores
 NULL
 
-
-#' @rdname elementaryscores
-#' @export
-es_expect <- function(a, t, x, y, right = FALSE) {
-  a <- as.vector(a, "numeric")
-  a[a <= 0 | a >= 1] <- NaN
+es_genquant <- function(expr, t, x, y, level, right) {
   t <- as.vector(t, "numeric")
   x <- as.matrix(x)
   y <- as.vector(y, "numeric")
+  level <- as.vector(level, "numeric")
   right <- as.vector(right, "logical")
   
-  na <- length(a)
-  nt <- length(t)
-  nx <- dim(x)[2L]
-  ny <- length(y)
+  stopifnot(identical(dim(x)[1L], length(y)))
+  stopifnot(identical(length(level), 1L))
+  stopifnot(level > 0, level < 1)
   stopifnot(identical(length(right), 1L))
-  if (!identical(dim(x)[1L], ny))
-    stop("forecast matrix is incompatible with observation vector")
   
-  outdim <- c(ny, nx, nt, na)
-  if (!identical(na, 1L)) a <- rep(a, each = ny * nx * nt)
-  if (!identical(nt, 1L)) t <- rep(t, each = ny * nx)
+  dout <- c(length(t), dim(x))
   dim(x) <- NULL
-  
-  tx <- if (right) t <= x else t < x
-  ty <- if (right) t <= y else t < y
-  I <- ((y < x) - a) * abs(t - y)
-  
-  array(2 * I * (tx - ty), outdim)
+  if (!identical(dout[1], 1L)) {
+    x <- rep(x, rep.int(dout[1], length(x)))
+    y <- rep(y, rep.int(dout[1], length(y)))
+  }
+  xlt <- if (right) (x <= t) else (x < t)
+  ylt <- if (right) (y <= t) else (y < t)
+  array(eval(expr), dout)
+}
+
+
+#' @rdname elementaryscores
+#' @export
+es_expect <- function(t, x, y, level, right = FALSE) {
+  expr <- expression(abs(4 * (ylt - level) * (t - y) * (ylt - xlt)))
+  es_genquant(expr, t, x, y, level, right)
 }
 
 
 #' @rdname elementaryscores
 #' @export es_mean
 es_mean <- function(t, x, y, right = FALSE) {
-  t <- as.vector(t, "numeric")
-  x <- as.matrix(x)
-  y <- as.vector(y, "numeric")
-  right <- as.vector(right, "logical")
-  
-  nt <- length(t)
-  nx <- dim(x)[2L]
-  ny <- length(y)
-  stopifnot(identical(length(right), 1L))
-  if (!identical(dim(x)[1L], ny))
-    stop("forecast matrix is incompatible with observation vector")
-  
-  outdim <- c(ny, nx, nt)
-  if (!identical(nt, 1L)) t <- rep(t, each = ny * nx)
-  dim(x) <- NULL
-  
-  tx <- if (right) t <= x else t < x
-  ty <- if (right) t <= y else t < y
-  I <- abs(t - y)
-  
-  array(2 * I * (tx - ty), outdim)
+  expr <- expression(2 * (t - y) * (ylt - xlt))
+  es_genquant(expr, t, x, y, 0.5, right)
 }
 
 
 #' @rdname elementaryscores
 #' @export es_median
 es_median <- function(t, x, y, right = FALSE) {
-  t <- as.vector(t, "numeric")
-  x <- as.matrix(x)
-  y <- as.vector(y, "numeric")
-  right <- as.vector(right, "logical")
-  
-  nt <- length(t)
-  nx <- dim(x)[2L]
-  ny <- length(y)
-  stopifnot(identical(length(right), 1L))
-  if (!identical(dim(x)[1L], ny))
-    stop("forecast matrix is incompatible with observation vector")
-  
-  outdim <- c(ny, nx, nt)
-  if (!identical(nt, 1L)) t <- rep(t, each = ny * nx)
-  dim(x) <- NULL
-  
-  tx <- if (right) t <= x else t < x
-  ty <- if (right) t <= y else t < y
-  
-  array(2 * (tx - ty), outdim)
+  expr <- expression(abs(ylt - xlt))
+  es_genquant(expr, t, x, y, 0.5, right)
 }
 
 
 #' @rdname elementaryscores
 #' @export es_prob
 es_prob <- function(t, x, y, right = FALSE) {
-  t <- as.vector(t, "numeric")
   x <- as.matrix(x)
   x[x < 0 | x > 1] <- NaN
   y <- as.vector(y, "numeric")
   y[y != 0 & y != 1] <- NaN
-  right <- as.vector(right, "logical")
-  
-  nt <- length(t)
-  nx <- dim(x)[2L]
-  ny <- length(y)
-  stopifnot(identical(length(right), 1L))
-  if (!identical(dim(x)[1L], ny))
-    stop("forecast matrix is incompatible with observation vector")
-  
-  outdim <- c(ny, nx, nt)
-  if (!identical(nt, 1L)) t <- rep(t, each = ny * nx)
-  dim(x) <- NULL
-  
-  tx <- if (right) t <= x else t < x
-  ty <- if (right) t <= y else t < y
-  I <- abs(t - y)
-  
-  array(2 * I * (tx - ty), outdim)
+  es_mean(t, x, y, right)
 }
 
 
 #' @rdname elementaryscores
 #' @export es_quant
-es_quant <- function(a, t, x, y, right = FALSE) {
-  a <- as.vector(a, "numeric")
-  a[a <= 0 | a >= 1] <- NaN
-  t <- as.vector(t, "numeric")
-  x <- as.matrix(x)
-  y <- as.vector(y, "numeric")
-  right <- as.vector(right, "logical")
-  
-  na <- length(a)
-  nt <- length(t)
-  nx <- dim(x)[2L]
-  ny <- length(y)
-  stopifnot(identical(length(right), 1L))
-  if (!identical(dim(x)[1L], ny))
-    stop("forecast matrix is incompatible with observation vector")
-  
-  outdim <- c(ny, nx, nt, na)
-  if (!identical(na, 1L)) a <- rep(a, each = ny * nx * nt)
-  if (!identical(nt, 1L)) t <- rep(t, each = ny * nx)
-  dim(x) <- NULL
-  
-  tx <- if (right) t <= x else t < x
-  ty <- if (right) t <= y else t < y
-  I <- (y < x) - a
-  
-  array(2 * I * (tx - ty), outdim)
+es_quant <- function(t, x, y, level, right = FALSE) {
+  expr <- expression(2 * (ylt - level) * (ylt - xlt))
+  es_genquant(expr, t, x, y, level, right)
 }
